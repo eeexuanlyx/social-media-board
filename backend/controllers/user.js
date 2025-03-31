596f9367661a69d24c6ebb9dd6ae532969271984
eeexuanlyx <eeexuanlyx@gmail.com>
2025-03-31 22:23:19 +0800

    fetch profile info, posts by user and likes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,7 +11,7 @@ export const getUserProfile = async (req, res) => {
     const user = await User.findOne({ username }).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    res.status(200).json({ user });
+    res.status(200).json(user);
   } catch (error) {
     console.log("Error in getUserProfile", error.message);
     res.status(500).json({ message: error.message });
